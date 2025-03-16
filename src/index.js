9e4e0980bea6216ab187aca89d876ab301cc3def
jedidiah <jedaqsaul@gmail.com>
2025-03-16 12:15:13 +0300

    Add event listener to the form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const inputField = document.getElementById("new-task-description");
+    const taskTest = inputField.value.trim();
+
+    if (taskTest !== "") {
+      const listItem = document.createElement("li");
+      listItem.textContent = taskTest;
+
+      taskList.appendChild(listItem);
+      inputField.value = "";
+    }
+  });
 });
